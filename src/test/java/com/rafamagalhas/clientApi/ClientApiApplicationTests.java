4f4345a8a63902b4c2dd81e80de9e4efa2a02e85
rafamagalhas <magalhas.rafa@gmail.com>
2020-08-17 11:18:31 -0300

    Added client api tests

---
+++ src/test/java/com/rafamagalhas/clientApi/ClientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafamagalhas.clientApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
